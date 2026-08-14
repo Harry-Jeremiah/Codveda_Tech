10e70d3c5f75aa56ac8595196ae1391e02cbb510
Harry-Jeremiah <katumbah@sheridancollege.ca>
2026-08-13 22:41:13 -0400

    To-do list app

--- Intermediate/to_do_list.py
+++ Intermediate/to_do_list.py
@@ -9,16 +9,41 @@ Objectives:
     Implement basic error handling (trying to delete a task that doesn't exist)a
 
 """
+import json
+
 
 print("===== TO-DO LIST =====")
 print()
 
 tasks = []
 
+#load tasks from json file
+def load_tasks():
+    global tasks
+
+    try:
+        with open("tasks.json", "r") as file:
+            tasks = json.load(file)
+
+    except FileNotFoundError:
+        tasks = []
+
+#save the tasks in json file
+def save_task():
+    with open("tasks.json", "w") as file:
+        json.dump(tasks, file, indent=4)
+
 #add tasks
 def add_task():
     task = input("Enter the task you want to add: ")
-    tasks.append(task)
+    new_task = {
+        "task": task,
+        "Completed": False
+    }
+
+    tasks.append(new_task)
+
+    save_task()
 
     print("Your task has been added to the list")
 
@@ -30,15 +55,24 @@ def view_tasks():
     else:
         print("These are your tasks:")
         for number, task in enumerate(tasks, start=1):
-            print(f"{number}. {task}")
+            if task["Completed"]:
+                status = "Complete"
+            else:
+                status = "Not complete"
+            print(f"{number}. {task['task']} - {status}")
+
 
 #delete tasks
 def delete_task():
+    view_tasks()
+
     try:
 
         task_del = int(input("Enter the number of the task you want to delete: "))
 
         task_del = tasks.pop(task_del - 1)
+
+        save_task()
 
         print("You have deleted ", task_del)
 
@@ -49,23 +83,55 @@ def delete_task():
     except IndexError:
         print("The task is not in your list.")
 
-print("You can perform the following tasks.\n")
-
-print("~"*25)
-print()
-print("1. Add taks\n2. View tasks\n3. Delete tasks\n4. Exit\n")
-print("~"*25)
-
-choice = int(input("Enter your desired option: "))
-
-if choice == 1:
-    add_task()
-elif choice == 2:
+#mark as complete
+def mark_complete():
     view_tasks()
-elif choice == 3:
-    delete_task()
-elif choice == 4:
-    print("Thank you. Goodbye")
-else:
-    print("Invalid choice, choose between 1 - 4")
+
+    try:
+        task_num = int(input("Enter the number of the task you want to mark as complete: "))
+
+        task = tasks[task_num -  1]
+
+        task["Completed"] = True
+
+        save_task()
+
+        print("Task is marked as complete.")
+
+    except ValueError:
+        print("Enter a valid number")
+    except IndexError:
+        print("The task is not in the list")
+
+load_tasks()
+
+while True:
+
+    print("You can perform the following tasks.\n")
+
+    print("~"*30)
+    print()
+    print("1. Add taks\n2. View tasks\n3. Delete tasks\n4. Mark as complete\n5. Exit\n")
+    print("~"*30
+      )
+
+    try:
+        choice = int(input("Enter your desired option: "))
+
+        if choice == 1:
+            add_task()
+        elif choice == 2:
+            view_tasks()
+        elif choice == 3:
+            delete_task()
+        elif choice == 4:
+            mark_complete()
+        elif choice == 5:
+            print("These are your final tasks.")
+            view_tasks()
+            break
+        else:
+            print("Invalid choice, choose between 1 - 4")
+    except ValueError:
+        print("Please enter a number between 1 and 5")
 
